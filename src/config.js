52e1f327a71a8534cc7cf7bd91bef4139c52dbc4
Shakzz890 <120597222+Shakzz890@users.noreply.github.com>
2026-01-07 13:07:32 +0800

    Add files via upload

--- src/config.js
+++ src/config.js
@@ -1,9 +1,11 @@
+// src/config.js
 export const TMDB_API_KEY = '4eea503176528574efd91847b7a302cc'; 
 export const TMDB_BASE_URL = 'https://api.themoviedb.org/3';
 export const IMG_URL = 'https://image.tmdb.org/t/p/original';
 export const POSTER_URL = 'https://image.tmdb.org/t/p/w300';
-export const PLACEHOLDER_IMG = '/logo.png'; // Make sure you have a logo.png in public folder
+export const PLACEHOLDER_IMG = '/logo.png'; 
 
+// Sandbox Policies (From your script)
 export const PROVIDER_SANDBOX_POLICY = {
     'vidsrc.to': "allow-scripts allow-same-origin allow-presentation allow-forms allow-pointer-lock",
     'vidsrc.me': "allow-scripts allow-same-origin allow-presentation allow-forms",
